029fa1ff92bea03a16851c3824e3a5d3dac94f0b
orineo1 <66064142+orineo1@users.noreply.github.com>
2023-03-11 22:59:18 +0200

    add init_vectors_and_matrix and edit calcualte_mse
    init_vectors_and_matrix  - Initializes the vectors and matrices required for the Kalman filter 
    calcualte_mse  - Change of the MSE calculation configuration for a specific forecast (the 3rd reading) and not for a set of forecasts (until the 3rd reading)

--- predictBGKalmnFilter.py
+++ predictBGKalmnFilter.py
@@ -67,35 +67,44 @@ def predict(Z,T,alpha,numb_pred):
     next_predictions = [int(Z@  np.linalg.matrix_power(T  , i)@ alpha ) for i in range(1,numb_pred+1)] 
     return next_predictions
 
+def init_vectors_and_matrix():
+    """
+    A function that initializes the initial variables for the Kalman filter
+    """
 
-# Define the initial state
-alpha = np.array([[0], [0]])
+    # Define the initial state
+    alpha = np.array([[0], [0]])
 
-# Define the initial covariance matrix
-P = np.array([[1, 0], [0, 1]])
+    # Define the initial covariance matrix
+    P = np.array([[1, 0], [0, 1]])
 
-# Define the state transition matrix
-T = np.array([[1, 1], [0, 1]])
+    # Define the state transition matrix
+    T = np.array([[1, 1], [0, 1]])
 
-# Define the measurement matrix
-Z = np.array([[1, 0]])
+    # Define the measurement matrix
+    Z = np.array([[1, 0]])
 
-# Define the measurement noise covariance matrix
-Epsilon = np.array([[1]])
+    # Define the measurement noise covariance matrix
+    Epsilon = np.array([[1]])
 
-# Define the process noise covariance matrix
-Eta = np.array([[0.1, 0], [0, 0.1]])
+    # Define the process noise covariance matrix
+    Eta = np.array([[0.1, 0], [0, 0.1]])
+    
+    return alpha,P,T,Z,Epsilon,Eta
 
+# The number of previous observations includes the current observation with which the transition matrix is updated
+number_of_obs_to_update = 9
 
-def main_cgm(collection_mongo, Z=Z,Epsilon=Epsilon,T=T,alpha=alpha,P=P,Eta=Eta,mse_calc=False,mse_df=[]):
+def main_cgm(collection_mongo,mse_calc=False,mse_df=[]): # , Z=Z,Epsilon=Epsilon,T=T,alpha=alpha,P=P,Eta=Eta
     """
     The central function, to implement the kalman prediction algorithm
-    Retrieves the last 30 readings, updates the values of the matrices and predicts as the requested number of times
+    Retrieves the last number_of_obs_to_update readings, updates the values of the matrices and predicts as the requested number of times
     """
+    alpha,P,T,Z,Epsilon,Eta = init_vectors_and_matrix()
     entries_df = mse_df
 
     if not mse_calc:
-        entries_df = read_obs_mongo(30,collection_mongo)
+        entries_df = read_obs_mongo(number_of_obs_to_update,collection_mongo)
     
     
     alpha,P  = calbration_start(entries_df,Z,Epsilon,T,alpha,P,Eta)
@@ -107,33 +116,40 @@ def main_cgm(collection_mongo, Z=Z,Epsilon=Epsilon,T=T,alpha=alpha,P=P,Eta=Eta,m
 
  ######################## MSE CALCULATIONS ########################
 
-def calcualte_mse(BG_level_df,iterations,numb_pred=2):
+def calcualte_mse(BG_level_df,iterations,numb_pred=2,which_reading=1):
     """"
     A function to calculate the mse of the algorithm
 
     BG_level_df: df from mongodb that contains the sugar data and dates
     iterations: Calculating the mse on how many predicted observations
     numb_pred: How many steps forward in sugar values will be considered in the mse calculation
-
+    which_reading : on which forecast to calculate the MSE (between 1 and numb_pred)
     """
+    global number_of_obs_to_update
+    # variable that contains the sum of the subtraction between the prediction and the actual result squared
     squared_sum_pred_vs_real = 0
-    
     for i in range(iterations):
-        starting_point=random.randint(1, len(BG_level_df)-31)
-        temp_bg_df = BG_level_df.iloc[starting_point:starting_point+30,:].sort_values(by="date",
-                                                                                    ascending=False) 
-
+        # random starting point selection - limited to an index of number_of_obs_to_update prior to the last observation (the predictions is calculated on number_of_obs_to_update observations)
+        starting_point=random.randint(1, len(BG_level_df)-(number_of_obs_to_update+1+numb_pred))
+        # Choice of the next number_of_obs_to_update readings
+        temp_bg_df = BG_level_df.iloc[starting_point:starting_point+number_of_obs_to_update,:].sort_values(by="date",
+                                                                                        ascending=False) 
+        # calculate the predictions and save the real םbservations
         predictions = np.array(main_cgm(None, Z=Z,Epsilon=Epsilon,T=T,alpha=alpha,P=P,Eta=Eta,
-            mse_calc=True,mse_df=temp_bg_df) [:numb_pred])
-        real_obs = np.array(BG_level_df.iloc[starting_point+30:starting_point+30+numb_pred,:]["sgv"])
-
+        mse_calc=True,mse_df=temp_bg_df) [which_reading-1])
+        real_obs = np.array(BG_level_df.iloc[starting_point+number_of_obs_to_update:starting_point+number_of_obs_to_update+numb_pred,:]["sgv"])[which_reading-1]
+            
+        # subtraction the distance between the forecast and the actual value squared  
         squared_sum_pred_vs_real+= ((predictions-real_obs)**2).sum()
-    
-    mse = squared_sum_pred_vs_real/iterations
-    return "The MSE over {} is {}".format(iterations,mse) 
+        
+    # average over all predictions
+    mse =  squared_sum_pred_vs_real/(iterations)  
+    return  "The MSE of  the {} reading ahead over {} iterations is {}".format(which_reading,iterations,mse)
 
 
 # # # variables to calcualte mse function
 # path_to_bg_level_df = "name_of_csv"
 # BG_level_df = pd.read_csv(path_to_bg_level_df+".csv")
-# calcualte_mse(BG_level_df,20000,2)
+# # calcualte_mse(BG_level_df,40000,numb_pred=5,which_reading=1)
+
+
