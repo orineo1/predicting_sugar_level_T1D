3620dbd61a877c22f9db1ed9366c808fd6b7979b
orineo1 <66064142+orineo1@users.noreply.github.com>
2023-03-12 21:19:57 +0200

    Correcting spelling mistakes

--- CGMbot.py
+++ CGMbot.py
@@ -16,7 +16,7 @@ import predictBGKalmnFilter as predKalman # Import functions from the predictBGK
 init_data_dict = {}
 
 # Open the text file for reading
-with open('info_for_the_pred_algorithem - ORI.txt', 'r') as file:
+with open('info_for_the_pred_algorithm - ORI.txt', 'r') as file:
     # Loop over each line in the file
     for line in file.readlines():
 
@@ -97,9 +97,9 @@ def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
     return True
 
 
-async def complx_snooze_finish(context: ContextTypes.DEFAULT_TYPE) -> None:
+async def complex_snooze_finish(context: ContextTypes.DEFAULT_TYPE) -> None:
     """"
-    A helper function for complx_snooze, and is part of the flow complx_snooze -> complx_snooze_is_in_condition->complx_snooze_finish.
+    A helper function for complex_snooze, and is part of the flow complex_snooze -> complex_snooze_is_in_condition->complex_snooze_finish.
     This function is the last in the sequence and its purpose is to restart the prediction algorithm and return to the user why the algorithm has started working again
     """
     job = context.job
@@ -107,36 +107,36 @@ async def complx_snooze_finish(context: ContextTypes.DEFAULT_TYPE) -> None:
 
     context.job_queue.run_repeating(predict,  interval=60*2+30, first=1, chat_id=chat_id, name=str(chat_id))
 
-    # If the current sugar value is higher than the value defined in complx_snooze, return a message
+    # If the current sugar value is higher than the value defined in complex_snooze, return a message
     if predKalman.currnet_bg_val(collection_mongo,1)>high_val:
-        text = "BG level is going over then {}, prediction algorithem set again".format(high_val)
+        text = "BG level is going over then {}, prediction algorithm set again".format(high_val)
         await  context.bot.send_message(chat_id, text=text)
         return
 
     # In the case that the sugar value is not higher than the upper limit set and is probably lower than 140, return the appropriate message
-    text = "BG level is going under then 140, the  algorithem set again"
+    text = "BG level is going under then 140, the  algorithm set again"
     await  context.bot.send_message(chat_id, text=text)
 
-async def complx_snooze_is_in_condition(context: ContextTypes.DEFAULT_TYPE) -> None:
+async def complex_snooze_is_in_condition(context: ContextTypes.DEFAULT_TYPE) -> None:
     """
-    A helper function for complx_snooze, and is part of the flow complx_snooze -> complx_snooze_is_in_condition->complx_snooze_finish.
+    A helper function for complex_snooze, and is part of the flow complex_snooze -> complex_snooze_is_in_condition->complex_snooze_finish.
 
     The purpose of the function is to see if the condition of the complex snooze is still met -
-    that is, whether the sugar values are above 140 and below the target value defined when running the complx_snooze (the value from which we want to run the prediction algorithm again).
+    that is, whether the sugar values are above 140 and below the target value defined when running the complex_snooze (the value from which we want to run the prediction algorithm again).
     """
     job = context.job
     high_val ,chat_id = job.data
 
     curr_val = predKalman.currnet_bg_val(collection_mongo,1)
 
-    # If the sugar level is in the desired range (between 140 and the upper limit set in complx_snooze, continue in the loop - The complex snooze conditions still apply
+    # If the sugar level is in the desired range (between 140 and the upper limit set in complex_snooze, continue in the loop - The complex snooze conditions still apply
     while curr_val<high_val and curr_val>140 :
         await asyncio.sleep(2*60+30)
         # print("waited 2.5 min")
         curr_val = predKalman.currnet_bg_val(collection_mongo,1)
     
-    # If the threshold is crossed in either direction, continue in flow to end the complex snooze to the function complx_snooze_finish
-    context.job_queue.run_once(complx_snooze_finish, 1, chat_id=chat_id, name=str(chat_id), data=[high_val,chat_id] )
+    # If the threshold is crossed in either direction, continue in flow to end the complex snooze to the function complex_snooze_finish
+    context.job_queue.run_once(complex_snooze_finish, 1, chat_id=chat_id, name=str(chat_id), data=[high_val,chat_id] )
 
 ######################## THE MAIN COMMANDS OF THE BOT ########################
 
@@ -148,10 +148,10 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 The purpose of the bot is to predict sugar levels and update the user with a message if the BG levels are going to go out of the desired range <a href='https://github.com/orineo1/predicting_sugar_level_T1D'>(Github page)</a>\n
 <b>Bot Commands</b>
 /start or /help -> show this message.
-/start_predict -> starts running the prediction algorithem.
-/stop_predict -> stops the prediction algorithem.
-/simple_snooze -> stops the algorithem for 30 minutes and then excutes it again.
-/complx_snooze 'number' -> stops the notifications until reaching the 'number' or getting bellow 140.
+/start_predict -> starts running the prediction algorithm.
+/stop_predict -> stops the prediction algorithm.
+/simple_snooze -> stops the algorithm for 30 minutes and then excutes it again.
+/complex_snooze 'number' -> stops the notifications until reaching the 'number' or getting bellow 140.
 /next_bg -> prediction for the next 3 readings.
 /last_5_bg -> sugar values for the previous 5 readings.
 """, parse_mode='HTML',disable_web_page_preview=True)
@@ -177,7 +177,7 @@ async def start_pred_algorithm(update: Update, context = ContextTypes.DEFAULT_TY
     context.job_queue.run_repeating(predict,  interval=60*2+30, first=2, chat_id=chat_id, name=str(chat_id))
     
     # Updating the user on the start of the algorithm
-    text = "Start predection algorithem"
+    text = "Start prediction algorithm"
     if job_removed:
         text += ". Old one was removed."
     await update.message.reply_text(text)
@@ -191,14 +191,14 @@ async def terminate_pred_algorithm (update: Update, context: ContextTypes.DEFAUL
     job_removed = remove_job_if_exists(str(chat_id), context)
 
     # Updating the user
-    text = "Predection successfully cancelled!" if job_removed else "You have no active predection algorithem running."
+    text = "prediction successfully cancelled!" if job_removed else "You have no active prediction algorithm running."
     await update.message.reply_text(text)
 
 
-async def complx_snooze(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def complex_snooze(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """"
     snooze for the user who does not disturb him until reaching an upper limit that the user types or until reaching below 140.
-    Function as part of the flow complx_snooze -> complx_snooze_is_in_condition->complx_snooze_finish.
+    Function as part of the flow complex_snooze -> complex_snooze_is_in_condition->complex_snooze_finish.
     """
     global total_pred
     chat_id = update.message.chat_id
@@ -207,13 +207,13 @@ async def complx_snooze(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     # Checking whether the user has given an upper limit - if not return an error message
     try:high_border = int(context.args[0])
     except:
-        await update.message.reply_text("didn't recive any values for the higher border, next time call /complx_snooze <number>")
+        await update.message.reply_text("didn't recive any values for the higher border, next time call /complex_snooze <number>")
         return    
 
     # Alert on the limits and conditions for the snooze and start of the flow of the complex snooze
     text = "Set a snooze until the BG level is higher then {} or below 140".format(high_border) if job_removed else "You not have any job runnig"
     await update.message.reply_text(text)
-    context.job_queue.run_once(complx_snooze_is_in_condition, 1, chat_id=chat_id, name=str(chat_id), data=[high_border,update.message.chat_id])
+    context.job_queue.run_once(complex_snooze_is_in_condition, 1, chat_id=chat_id, name=str(chat_id), data=[high_border,update.message.chat_id])
 
 
 async def simple_snooze(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -223,7 +223,7 @@ async def simple_snooze(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     # If there is an active job (prediction algorithm) - delete it
     job_removed = remove_job_if_exists(str(chat_id), context)
 
-    # Returning an answer to the user and adding a task of starting the predections algorithm in another 30 minutes
+    # Returning an answer to the user and adding a task of starting the predictions algorithm in another 30 minutes
     text = "Set a snooze for 30 minutes" if job_removed else "You not have any job runnig"
     context.job_queue.run_repeating(start_pred_algorithm,  interval=60*2+30, first=30*60, chat_id=chat_id, name=str(chat_id))
     await update.message.reply_text(text)
@@ -275,7 +275,7 @@ def main() -> None:
     application.add_handler(CommandHandler("start_predict", start_pred_algorithm))
     application.add_handler(CommandHandler("stop_predict", terminate_pred_algorithm ))
     application.add_handler(CommandHandler("simple_snooze", simple_snooze))
-    application.add_handler(CommandHandler("complx_snooze", complx_snooze))
+    application.add_handler(CommandHandler("complex_snooze", complex_snooze))
     application.add_handler(CommandHandler("next_bg", next_bg))
     application.add_handler(CommandHandler("last_5_bg", last_5_bg))
 
